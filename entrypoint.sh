b39319ab79e2506759a2e90615f2076dc3fc7288
Diego Dorgam <dorgam@mapeo.ai>
2023-07-23 17:03:10 -0300

    load mongodb

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,28 +6,35 @@ cd /docker
 
 # Poll until we can successfully connect to MongoDB
 echo 'Connecting to MongoDB...'
-#node <<- 'EOJS'
-#require('p-wait-for')(function() {
-#	return new Promise(function (resolve, reject) {
-#		require('mongodb').MongoClient.connect(process.env.MONGO_URL, function(err, client) {
-#			const successfullyConnected = err == null;
-#			if (successfullyConnected) {
-#				client.close();
-#				process.exit(0);
-#			} else {
-#				if(client) {
-#					client.close();
-#				}
-#				resolve(false);
-#				return;
-#			}
-#			resolve(successfullyConnected);
-#		});
-#	});
-#}, 10000);
-#EOJS
+node <<- 'EOJS'
+const pWaitFor = (...args) => import('p-wait-for').then(
+        ((pWaitFor) => {
+                pWaitFor(()=>{
+                        return new Promise(function (resolve, reject) {
+                                require('mongodb').MongoClient.connect(process.env.MONGO_URL, function(err, client) {
+                                        const successfullyConnected = err == null;
+                                        if (successfullyConnected) {
+                                                client.close();
+                                                process.exit(0);
+                                        } else {
+                                                if(client) {
+                                                        client.close();
+                                                }
+                                                resolve(false);
+                                                return;
+                                        }
+                                        resolve(successfullyConnected);
+                                });
+                        });
+                },
+        {
+            "interval":1000
+        });
+        })
+)
+EOJS
 
-node waitmongo.mjs
+# node waitmongo.mjs
 
 echo 'Starting app...'
 cd /opt/bundle/bundle
